308dcb1ff8fdacd5dc1846a07a4b9ae6f30f928f
hrzaa <harezayoan@gmail.com>
2022-02-22 20:07:28 +0700

    controller

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+    public function index($nama = 'hareza', $pekerjaan = 'gamer')
+    {
+        echo "nama saya $nama, saya sebagai $pekerjaan";
+    }
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
