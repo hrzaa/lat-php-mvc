b9481d2b20e303b6c941147e3dc362061038c969
hrzaa <harezayoan@gmail.com>
2022-02-23 13:28:48 +0700

    view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About
+class About extends controller
 {
-    public function index($nama = 'hareza', $pekerjaan = 'gamer')
+    public function index($nama = 'hareza', $pekerjaan = 'gamer', $umur = '19')
     {
-        echo "nama saya $nama, saya sebagai $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'My Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,2 @@
+<h1>About Me</h1>
+<p>Halo nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan']; ?>, umur saya <?= $data['umur']; ?> tahun.</p>
